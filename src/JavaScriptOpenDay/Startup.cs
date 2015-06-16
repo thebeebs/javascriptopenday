7afdcfbd8ad9b762efd0d2891dbb6ec35f3144ff
Martin Beeby <martinbeeby@hotmail.com>
2015-06-16 15:41:24 +0100

    Adding MVC

--- src/JavaScriptOpenDay/Startup.cs
+++ src/JavaScriptOpenDay/Startup.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Builder;
+using Microsoft.AspNet.Hosting;
 using Microsoft.AspNet.Http;
 using Microsoft.Framework.DependencyInjection;
 
@@ -13,12 +14,26 @@ namespace JavaScriptOpenDay
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
-        public void Configure(IApplicationBuilder app)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            app.UseStaticFiles();
+            // Add the following to the request pipeline only in development environment.
+            if (env.IsEnvironment("Development"))
+            {
+                app.UseBrowserLink();
+            }
             
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller}/{action}/{id?}",
+                    defaults: new { controller = "Home", action = "Index" });
+            });
+app.UseStaticFiles();
         }
     }
 }
