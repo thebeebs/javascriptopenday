4d9bc37be459b4b70644c60961de45762fe7c9d8
Martin Kearn <martin.kearn@microsoft.com>
2015-08-12 13:59:33 +0100

    Added ChatMessage entity

---
+++ src/Domain/Entities/ChatMessage.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class ChatMessage
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Message { get; set; }
+
+        public int ParentThreadId { get; set; }
+    }
+}
